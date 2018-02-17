9956da6f9e705776cbd27d346034fa9154068750
isaiahgregov <isaiahgregov@icloud.com>
2018-02-17 07:09:26 -0600

    I can't get the GUI to work, but if you load the .fxml file, you can see in the Scene Builder hierarchy the controls added that we will use to control the locations of the pieces.

--- TeamLewisChess/src/teamlewischess/TeamLewisChess.java
+++ TeamLewisChess/src/teamlewischess/TeamLewisChess.java
@@ -1,27 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// TeamLewisChess.java
+// Main application class that loads and displays Team Lewis's chess GUI.
 package teamlewischess;
 
-/**
- *
- * 
- * Test again 23 asdf
- * 
- * @author grano
- * 
- * Another Test
- * 
- */
-public class TeamLewisChess {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+public class TeamLewisChess extends Application {
+   @Override
+   public void start(Stage stage) throws Exception {
+      Parent root = 
+         FXMLLoader.load(getClass().getResource("TeamLewisChess.fxml"));
+
+      Scene scene = new Scene(root); // attach scene graph to scene
+      stage.setTitle("Team Lewis Chess"); // displayed in window's title bar
+      stage.setScene(scene); // attach scene to stage
+      stage.show(); // display the stage
+   }
+
+   public static void main(String[] args) {
+      // create a TeamLewisChess object and call its start method
+      launch(args); 
+   } 
 }
