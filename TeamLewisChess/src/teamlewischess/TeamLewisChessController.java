96546d2a9c75264e8a7b4b00589c4a7cb1ce3f45
isaiahgregov <isaiahgregov@icloud.com>
2018-02-17 07:14:23 -0600

    I can't get the GUI to work, but if you load the .fxml file, you can see in the Scene Builder hierarchy the controls added that we will use to control the locations of the pieces.

---
+++ TeamLewisChess/src/teamlewischess/TeamLewisChessController.java
@@ -0,0 +1,395 @@
+package teamlewischess;
+
+// TeamLewisChessController.java
+// Controller that handles Rectangle and ImageView events
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
+
+public class TeamLewisChessController {
+
+    @FXML
+    private Rectangle A8_Rectangle;
+
+    @FXML
+    private Rectangle A7_Rectangle;
+
+    @FXML
+    private Rectangle A6_Rectangle;
+
+    @FXML
+    private Rectangle A5_Rectangle;
+
+    @FXML
+    private Rectangle A4_Rectangle;
+
+    @FXML
+    private Rectangle A3_Rectangle;
+
+    @FXML
+    private Rectangle A2_Rectangle;
+
+    @FXML
+    private Rectangle A1_Rectangle;
+
+    @FXML
+    private Rectangle B8_Rectangle;
+
+    @FXML
+    private Rectangle B7_Rectangle;
+
+    @FXML
+    private Rectangle B6_Rectangle;
+
+    @FXML
+    private Rectangle B5_Rectangle;
+
+    @FXML
+    private Rectangle B4_Rectangle;
+
+    @FXML
+    private Rectangle B3_Rectangle;
+
+    @FXML
+    private Rectangle B2_Rectangle;
+
+    @FXML
+    private Rectangle B1_Rectangle;
+
+    @FXML
+    private Rectangle C8_Rectangle;
+
+    @FXML
+    private Rectangle C7_Rectangle;
+
+    @FXML
+    private Rectangle C6_Rectangle;
+
+    @FXML
+    private Rectangle C5_Rectangle;
+
+    @FXML
+    private Rectangle C4_Rectangle;
+
+    @FXML
+    private Rectangle C3_Rectangle;
+
+    @FXML
+    private Rectangle C2_Rectangle;
+
+    @FXML
+    private Rectangle C1_Rectangle;
+
+    @FXML
+    private Rectangle D8_Rectangle;
+
+    @FXML
+    private Rectangle D7_Rectangle;
+
+    @FXML
+    private Rectangle D6_Rectangle;
+
+    @FXML
+    private Rectangle D5_Rectangle;
+
+    @FXML
+    private Rectangle D4_Rectangle;
+
+    @FXML
+    private Rectangle D3_Rectangle;
+
+    @FXML
+    private Rectangle D2_Rectangle;
+
+    @FXML
+    private Rectangle D1_Rectangle;
+
+    @FXML
+    private Rectangle E8_Rectangle;
+
+    @FXML
+    private Rectangle E7_Rectangle;
+
+    @FXML
+    private Rectangle E6_Rectangle;
+
+    @FXML
+    private Rectangle E5_Rectangle;
+
+    @FXML
+    private Rectangle E4_Rectangle;
+
+    @FXML
+    private Rectangle E3_Rectangle;
+
+    @FXML
+    private Rectangle E2_Rectangle;
+
+    @FXML
+    private Rectangle E1_Rectangle;
+
+    @FXML
+    private Rectangle F8_Rectangle;
+
+    @FXML
+    private Rectangle F7_Rectangle;
+
+    @FXML
+    private Rectangle F6_Rectangle;
+
+    @FXML
+    private Rectangle F5_Rectangle;
+
+    @FXML
+    private Rectangle F4_Rectangle;
+
+    @FXML
+    private Rectangle F3_Rectangle;
+
+    @FXML
+    private Rectangle F2_Rectangle;
+
+    @FXML
+    private Rectangle F1_Rectangle;
+
+    @FXML
+    private Rectangle G8_Rectangle;
+
+    @FXML
+    private Rectangle G7_Rectangle;
+
+    @FXML
+    private Rectangle G6_Rectangle;
+
+    @FXML
+    private Rectangle G5_Rectangle;
+
+    @FXML
+    private Rectangle G4_Rectangle;
+
+    @FXML
+    private Rectangle G3_Rectangle;
+
+    @FXML
+    private Rectangle G2_Rectangle;
+
+    @FXML
+    private Rectangle G1_Rectangle;
+
+    @FXML
+    private Rectangle H8_Rectangle;
+
+    @FXML
+    private Rectangle H7_Rectangle;
+
+    @FXML
+    private Rectangle H6_Rectangle;
+
+    @FXML
+    private Rectangle H5_Rectangle;
+
+    @FXML
+    private Rectangle H4_Rectangle;
+
+    @FXML
+    private Rectangle H3_Rectangle;
+
+    @FXML
+    private Rectangle H2_Rectangle;
+
+    @FXML
+    private Rectangle H1_Rectangle;
+
+    @FXML
+    private ImageView A8_ImageView;
+
+    @FXML
+    private ImageView A7_ImageView;
+
+    @FXML
+    private ImageView A6_ImageView;
+
+    @FXML
+    private ImageView A5_ImageView;
+
+    @FXML
+    private ImageView A4_ImageView;
+
+    @FXML
+    private ImageView A3_ImageView;
+
+    @FXML
+    private ImageView A2_ImageView;
+
+    @FXML
+    private ImageView A1_ImageView;
+
+    @FXML
+    private ImageView B8_ImageView;
+
+    @FXML
+    private ImageView B7_ImageView;
+
+    @FXML
+    private ImageView B6_ImageView;
+
+    @FXML
+    private ImageView B5_ImageView;
+
+    @FXML
+    private ImageView B4_ImageView;
+
+    @FXML
+    private ImageView B3_ImageView;
+
+    @FXML
+    private ImageView B2_ImageView;
+
+    @FXML
+    private ImageView B1_ImageView;
+
+    @FXML
+    private ImageView C8_ImageView;
+
+    @FXML
+    private ImageView C7_ImageView;
+
+    @FXML
+    private ImageView C6_ImageView;
+
+    @FXML
+    private ImageView C5_ImageView;
+
+    @FXML
+    private ImageView C4_ImageView;
+
+    @FXML
+    private ImageView C3_ImageView;
+
+    @FXML
+    private ImageView C2_ImageView;
+
+    @FXML
+    private ImageView C1_ImageView;
+
+    @FXML
+    private ImageView D8_ImageView;
+
+    @FXML
+    private ImageView D7_ImageView;
+
+    @FXML
+    private ImageView D6_ImageView;
+
+    @FXML
+    private ImageView D5_ImageView;
+
+    @FXML
+    private ImageView D4_ImageView;
+
+    @FXML
+    private ImageView D3_ImageView;
+
+    @FXML
+    private ImageView D2_ImageView;
+
+    @FXML
+    private ImageView D1_ImageView;
+
+    @FXML
+    private ImageView E8_ImageView;
+
+    @FXML
+    private ImageView E7_ImageView;
+
+    @FXML
+    private ImageView E6_ImageView;
+
+    @FXML
+    private ImageView E5_ImageView;
+
+    @FXML
+    private ImageView E4_ImageView;
+
+    @FXML
+    private ImageView E3_ImageView;
+
+    @FXML
+    private ImageView E2_ImageView;
+
+    @FXML
+    private ImageView E1_ImageView;
+
+    @FXML
+    private ImageView F8_ImageView;
+
+    @FXML
+    private ImageView F7_ImageView;
+
+    @FXML
+    private ImageView F6_ImageView;
+
+    @FXML
+    private ImageView F5_ImageView;
+
+    @FXML
+    private ImageView F4_ImageView;
+
+    @FXML
+    private ImageView F3_ImageView;
+
+    @FXML
+    private ImageView F2_ImageView;
+
+    @FXML
+    private ImageView F1_ImageView;
+
+    @FXML
+    private ImageView G8_ImageView;
+
+    @FXML
+    private ImageView G7_ImageView;
+
+    @FXML
+    private ImageView G6_ImageView;
+
+    @FXML
+    private ImageView G5_ImageView;
+
+    @FXML
+    private ImageView G4_ImageView;
+
+    @FXML
+    private ImageView G3_ImageView;
+
+    @FXML
+    private ImageView G2_ImageView;
+
+    @FXML
+    private ImageView G1_ImageView;
+
+    @FXML
+    private ImageView H8_ImageView;
+
+    @FXML
+    private ImageView H7_ImageView;
+
+    @FXML
+    private ImageView H6_ImageView;
+
+    @FXML
+    private ImageView H5_ImageView;
+
+    @FXML
+    private ImageView H4_ImageView;
+
+    @FXML
+    private ImageView H3_ImageView;
+
+    @FXML
+    private ImageView H2_ImageView;
+
+    @FXML
+    private ImageView H1_ImageView;
+
+}
