661a774e861fdcdf03b0ca352bb2ae4a61ad79de
haegerp <adrienne.haeger@usm.edu>
2018-04-05 21:01:43 -0500

    Valid Knight moves are in.  square occupied and king in check not incorporated. Left commented debug statements in for now until validated

--- TeamLewisChess/src/teamlewischess/Knight.java
+++ TeamLewisChess/src/teamlewischess/Knight.java
@@ -1,5 +1,8 @@
 package teamlewischess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @see Piece
  */
@@ -10,10 +13,26 @@ public class Knight extends Piece {
      * @param fTeam
      * @return 
      */
-    static public boolean isValidMove(int[][] fPieceTypeLocationsOnBoard, Team fTeam)
-    {
+    static public boolean isValidMove(int[][] fPieceTypeLocationsOnBoard, Team fTeam) {
+//         System.out.println("Knight withinRange of PieceMobility: " + 
+//                 Knight.withinRangeOfPieceMobility(fPieceTypeLocationsOnBoard));
+//         System.out.println("noPieceBlocksPathToSquare: " + 
+//                 Knight.noPieceBlocksPathToSquare(fPieceTypeLocationsOnBoard));
+//         System.out.println("moveDoesNotPlaceKingInCheck:  " + 
+//                 Knight.moveDoesNotPlaceKingInCheck(fPieceTypeLocationsOnBoard));
+                 
+        if(Knight.withinRangeOfPieceMobility(fPieceTypeLocationsOnBoard)){
+            
+            if(Knight.noPieceBlocksPathToSquare(fPieceTypeLocationsOnBoard)){
+            
+                if(Knight.moveDoesNotPlaceKingInCheck(fPieceTypeLocationsOnBoard)){
+                    
+                    return true;
+                }
+            }
+        }
         //Just to satisfy program and to be updated with algorithm:
-        return true;
+        return false;
     }
     
     /**
@@ -21,9 +40,67 @@ public class Knight extends Piece {
      * @param fPieceTypeLocationsOnBoard
      * @return 
      */
-    static private boolean withinRangeOfPieceMobility(int[][] fPieceTypeLocationsOnBoard)
-    {
-        //Just to satisfy program and to be updated with algorithm:
+    static private boolean withinRangeOfPieceMobility(int[][] fPieceTypeLocationsOnBoard) {
+        final int SIZE = 8;
+        final int MAX_ROW = 7;
+        final int MIN_ROW = 0;
+        final int MAX_COL = 7;
+        final int MIN_COL = 0;
+        final List<String> legalMoves = new ArrayList<>();
+        final List<Integer> legalSquares = new ArrayList<>();
+
+        int squareTargeted = Game.getTargetedSquare();
+        int squareClicked = TeamLewisChessController.getSquareClicked();
+         //System.out.println("squareTargeted = " + squareTargeted);
+         //System.out.println("squareClicked = " + squareClicked);
+         int cindx = Board.getColumnFromLocation(squareTargeted);
+         int rindx = Board.getRowFromLocation(squareTargeted);
+         String moveString;
+         int squareNum=-1;
+         //Just to satisfy program and to be updated with algorithm:
+        for ( int r = rindx - 2; r < rindx +3 ; r=r+4) {
+           
+            for (int c = cindx -1; c < cindx +2; c+=2) {
+                //System.out.print("rindx, cindx = " + rindx + "," + cindx + ": =  " + r + ", " +c);
+                if(r >=MIN_ROW && r <= MAX_ROW && c >= MIN_COL && c <= MAX_COL) {
+                    //System.out.println("");
+                    moveString = "r"+r+"c"+c;
+                    legalMoves.add(moveString);
+                    squareNum=c+(r*SIZE);
+                   
+                    
+                    legalSquares.add(c+(r*SIZE));
+
+                } else {
+                    //System.out.println(" illegal move");
+                
+                }
+            }
+        }
+        for ( int r = rindx -1; r< rindx +2; r+=2) {
+            for (int c = cindx-2; c < cindx + 3; c+=4) {
+                //System.out.print("rindx, cindx = " + rindx + "," + cindx + ": =  " + r + ", " +c);
+                if(r >=MIN_ROW && r <= MAX_ROW && c >= MIN_COL && c <= MAX_COL) {
+                    //System.out.println("");
+                    moveString = "r"+r+"c"+c;
+                    legalMoves.add(moveString);
+                    legalSquares.add(c+(r*SIZE));
+                    
+                } else {
+                    //System.out.println(" illegal move");
+                
+                }
+            }
+            
+        }
+        
+        for (int lm : legalSquares) {
+            //System.out.println("Legal Square = " + lm);
+            if(squareClicked==lm) {
+                //System.out.println("match found, can move if empty or opponent on square");
+                return true;
+            }
+        }
         return false;
     }
     
@@ -35,7 +112,8 @@ public class Knight extends Piece {
     static private boolean noPieceBlocksPathToSquare(int[][] fPieceTypeLocationsOnBoard)
     {
         //Just to satisfy program and to be updated with algorithm:
-        return false;
+        //return false;
+        return true;
     }
     
     /**
@@ -46,7 +124,9 @@ public class Knight extends Piece {
     static private boolean moveDoesNotPlaceKingInCheck(int[][] fPieceTypeLocationsOnBoard)
     {
         //Just to satisfy program and to be updated with algorithm:
-        return false;
+        //return false;
+        return true;
+
     }
     /**
      * 
